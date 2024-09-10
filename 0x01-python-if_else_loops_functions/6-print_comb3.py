4dee8026e2ba1275beed9428b87798b111516f3a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-11 01:17:23 +0300

    day 2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    last_digit = i % 10
-    first_digit = (i - last_digit) / 10
-    interchanged = (last_digit * 10) + first_digit
-    if i < interchanged:
-        print(i)
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:02}, ".format(i * 10 + j), end="")
+print("{:02}".format(89))
