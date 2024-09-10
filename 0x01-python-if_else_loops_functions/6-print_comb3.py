2c7e4fe1e181bce44bbb1964e1d6a28fac93a702
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-11 01:21:24 +0300

    day 2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,8 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02}, ".format(i * 10 + j), end="")
-print("{:02}".format(89))
+        # Print numbers followed by a comma and space, except for the last one
+        if i == 8 and j == 9:
+            print("{:02}".format(i * 10 + j))
+        else:
+            print("{:02}, ".format(i * 10 + j), end="")
