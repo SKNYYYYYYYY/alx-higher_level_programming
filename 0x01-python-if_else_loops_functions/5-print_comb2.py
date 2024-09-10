a05a1cae008aad38d1456a5fc9108b4558f8e2c9
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-10 15:11:52 -0700

    day 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-result = []
-
 for i in range(0, 100):
-    if i < 10:
-        result.append("{:02}".format(i))
+    if i < 99:
+        print("{:02}".format(i), end=", ")
     else:
-        result.append("{}".format(i))
-
-# Join all elements with a comma and space, and print the result
-print(", ".join(result))
+        print("{:02}".format(i))
