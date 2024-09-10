6e3cd5233fcdce9f2f4c7e5031408d360014e675
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-11 01:57:00 +0300

    day 2

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -8,7 +8,8 @@ def remove_char_at(s, n):
     n (int): The position of the character to remove.
 
     Returns:
-    str: A new string with the character at position n removed, or the original string if n is out of range.
+    str: A new string with the character at position n removed,
+    or the original string if n is out of range.
     """
     if n < 0 or n >= len(s):
         return s  # Return the original string if the position is out of range
