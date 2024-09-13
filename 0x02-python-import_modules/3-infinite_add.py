c180ec37b9864cde8ff113b9ecc202081382513e
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-13 04:53:55 +0300

    importing modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import sys
-sum = 0
-counter = 0
-num = len(sys.argv)
 
-while counter < num - 1:
-    sum += int(sys.argv[counter + 1])
-    counter += 1
-print(sum)
+
+def main():
+    sum = 0
+    counter = 0
+    num = len(sys.argv)
+
+    while counter < num - 1:
+        sum += int(sys.argv[counter + 1])
+        counter += 1
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
