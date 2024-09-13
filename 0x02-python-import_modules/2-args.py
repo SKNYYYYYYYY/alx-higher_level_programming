7ac4efe08e15b6bf67f38457e8ddd96eddb22859
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-13 04:49:47 +0300

    importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 import sys
-num = len(sys.argv)
-counter = 1
-if num  == 1:
-    print("{} argument.".format(num - 1))
-elif num > 1:
-    print("{} arguments:" .format(num - 1))
-    while counter < num:
-        print("{}: {}" .format(counter, sys.argv[counter]))
-        counter += 1
+
+
+def main():
+    num = len(sys.argv)
+    counter = 1
+    if num == 1:
+        print("{} argument.".format(num - 1))
+    elif num > 1:
+        print("{} arguments:" .format(num - 1))
+        while counter < num:
+            print("{}: {}" .format(counter, sys.argv[counter]))
+            counter += 1
+
+
+if __name__ == "__main__":
+    main()
