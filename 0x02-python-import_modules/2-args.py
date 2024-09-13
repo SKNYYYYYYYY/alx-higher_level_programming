cdc29bfafb401cd3463cd204fc7cc201ba09ce5b
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-13 05:01:27 +0300

    importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,12 @@ def main():
     num = len(sys.argv)
     counter = 1
     if num == 1:
-        print("{} argument.".format(num - 1))
-    elif num > 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
+        print("{}: {}" .format(counter, sys.argv[counter]))
+
+    elif num > 2:
         print("{} arguments:" .format(num - 1))
         while counter < num:
             print("{}: {}" .format(counter, sys.argv[counter]))
