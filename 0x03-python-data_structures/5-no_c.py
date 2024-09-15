03764f3558bcce438b2f4fdf9cbc948bc843048d
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-15 17:01:15 +0300

    data structures 101

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    #iterate while look for c and C then it removes it and continues
     new_string = []
     new = []
-    
     for i in my_string:
         if i not in ('c', 'C'):
             new_string.append(i)
