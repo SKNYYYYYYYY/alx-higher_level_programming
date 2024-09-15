8ee9f6521e3cded96389dadd125d91557889c068
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-15 17:34:26 +0300

    data structures 101

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return length, first
+    if sentence:
+        length = len(sentence)
+        first = sentence[0]
+        return length, first
