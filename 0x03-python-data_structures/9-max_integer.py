2551d2eab529878473a60319898612b25a22f843
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-15 17:38:35 +0300

    data structures 101

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    checker = 0
-    if len(my_list) == 0:
+    if my_list:    
+        checker = 0
+        if len(my_list) == 0:
+            return None
+        for i in my_list:
+            if i > checker:
+                checker = i
+        return checker
+    else:
         return None
-    for i in my_list:
-        if i > checker:
-            checker = i
-    return checker
