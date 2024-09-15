050f0b980c58443eba64bfb9016c52eb53f7f26f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-15 17:46:44 +0300

    data structures 101

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
+
     checker = my_list[0]  # Initialize checker to the first element of the list
     for i in my_list:
         if i > checker:
             checker = i  # Update checker if a larger integer is found
-            
+ 
     return checker
