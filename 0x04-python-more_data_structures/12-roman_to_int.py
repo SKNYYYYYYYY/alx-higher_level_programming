cd15990dd5633de59fbf3d8c9d13ba7afdc10d87
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-20 19:20:13 +0300

    DSA

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    n = len(roman_string)
+    i = n - 1
+    output = 0
+    while i >= 0:
+        if i < n - 1 and map[roman_string[i]] < map[roman_string[i + 1]]:
+            output -= map[roman_string[i]]
+        else:
+            output += map[roman_string[i]]
+        i -= 1
+    return output
