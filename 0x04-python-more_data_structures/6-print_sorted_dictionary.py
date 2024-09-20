d96d36a616d32db1f21057c0feb2cbf0c69f984a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-20 15:31:42 +0300

    DSA

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,3 @@ def print_sorted_dictionary(a_dictionary):
     for keys in sorted(a_dictionary):
         new_dictionary[keys] = a_dictionary[keys]
         print(f"{keys}: {a_dictionary[keys]}")
-    
