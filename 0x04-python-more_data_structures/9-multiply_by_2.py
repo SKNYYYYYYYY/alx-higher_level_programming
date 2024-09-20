55c406d32e6a74931fde4d94698ab8a6eca2b758
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-20 17:39:32 +0300

    DSA

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 def multiply_by_2(a_dictionary):
     dict = {}
     for key, value in a_dictionary.items():
-            value = value * 2
-            dict[key] = value
+        value = value * 2
+        dict[key] = value
     return dict
