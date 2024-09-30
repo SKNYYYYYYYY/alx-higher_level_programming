dc591f26478db43342277262763caf6d448a3b41
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-30 19:49:27 +0300

    Exception Handling

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    none_int = 0
+    try:
+        for i in my_list:
+            if counter < x  and isinstance(i, int):
+                print(i, end="")
+                counter += 1
+            else:
+                none_int += 1
+                continue
+        print()
+        if x - (counter + none_int) != 0 and counter != x:
+            raise IndexError
+    except IndexError:
+        print("IndexError: list index out of range")
+    return counter
