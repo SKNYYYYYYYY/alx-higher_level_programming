6ac1c3cae04a9234299da81d67c9642a5557e474
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-30 20:02:18 +0300

    Exception Handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,6 +13,6 @@ def safe_print_list_integers(my_list=[], x=0):
         print()
         if x > counter:
             raise IndexError 
-    except IndexError:
+    except ValueError:
         print("Error")
     return counter
