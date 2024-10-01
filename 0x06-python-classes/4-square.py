eaaa524e94a5cd80eac15e40947324460e8a905f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-01 22:40:42 +0300

    Empty Class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module contains a class that has a constructor"""
+try:
+    class Square:
+        """This class has a constructor"""
+        def __init__(self, size=0):
+            """This is a method that checks whether the size provided is a
+              positive integer
+            args: size, size parameter provided during calling
+            """
+            self.__size = size
+
+        @property
+        def size(self):
+            """Getter method for size"""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """Setter method for size"""
+            self.__size = value
+            if not isinstance(self.__size, int):
+                raise TypeError("size must be an integer")
+            if self.__size < 0:
+                raise ValueError("size must be >= 0")
+
+        def area(self):
+            """This function calculates the area of the square
+            Returns: float, area of the square
+            """
+            area = self.__size * self.__size
+            return area
+except TypeError as te:
+    print(te)
+except ValueError as ve:
+    print(ve)
