52426a9f30c3580725a18544645fef69a2ebf5fe
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-01 21:27:36 +0300

    Empty Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This module defines the Square class"""
 class Square:
+    """ This class has the square method that does nothing"""
     def square(self):
+        """ This method DOES NOT do anything"""
         pass
