3890896ce269cad8336f26d9a845bd2ff376d791
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-06 23:14:04 +0300

    Python test driven Development

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from typing import Type
+
+
+def matrix_divided(matrix, div):
+    new_matrix = []
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+
+
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    first_row_len = len(matrix[0])
+    for row in matrix:
+        if first_row_len != len(row):
+             raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for  i in row:
+                if (type(i) not in [int, float]):
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                else:
+                    quotte = round(i/div, 2)
+                    new_row.append(quotte)
+        new_matrix.append(new_row)
+
+    return new_matrix
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    output = []
+    space_flag = False
+    for i in range(len(text)):
+        if text[i] == ' ' and space_flag:
+            continue
+        output.append(text[i])
+        if text[i] in (":", "?", "."):
+            output.append("\n\n")
+            space_flag = True
+        else:
+            space_flag = False
+    print("".join(output))
