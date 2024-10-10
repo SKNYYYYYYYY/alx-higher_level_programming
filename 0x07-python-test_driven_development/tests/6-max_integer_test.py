ea514d08e738eb9568f7f009add2fc932876617a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-11 00:01:00 +0300

    unit testing and docstring

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,4 +6,8 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
-        self.assertIsInstance(list, list)
+        self.assertEqual(max_integer([1,2]), 2)
+        self.assertEqual(max_integer([0, 0]), 0)
+        self.assertEqual(max_integer([-1,-2]), -1)
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
