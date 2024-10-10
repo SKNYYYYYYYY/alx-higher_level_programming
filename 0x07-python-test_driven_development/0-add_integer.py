01cb0923c27992da7881be977b6c9f63c6e52a8c
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-10 22:21:45 +0300

    unit testing and docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""defines the integer addition function"""
+"""defines the integer addition function
+    checks if a and b is integer
+    checks if a is float
+    checks if b is a float
+"""
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats (cast to integers).
-
-    Args:
-        a (int, float): First number.
-        b (int, float, optional): Second number, defaults to 98.
-
-    Returns:
-        int: The sum of a and b, after converting floats to integers.
-
-    Raises:
-        TypeError: If a or b are not integers or floats.
+    """ Args: a and b; Returns: int: The sum of a and b, after converting floats to integers.
+    Raises: TypeError: If a or b are not integers or floats.
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
