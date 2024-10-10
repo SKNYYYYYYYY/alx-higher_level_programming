afac352cfe7cd1723f80997ce346dce57437c9b0
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-10 21:51:47 +0300

    unit testing and docstring

---
+++ 0x07-python-test_driven_development/my_module.py
@@ -0,0 +1,5 @@
+"""
+This module provides utility functions for basic mathematical operations.
+The module is designed to help with simple arithmetic and can be extended 
+for more complex use cases.
+"""
