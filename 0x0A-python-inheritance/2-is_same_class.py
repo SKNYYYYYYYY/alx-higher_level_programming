0a4036c270470c1fd614bde4fa811e4dbf56690d
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-14 22:53:29 +0300

    Inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,11 +3,13 @@
     This module defines the function is_same_class method
     that checks if the class is an instance of the specified class
 """
+
+
 def is_same_class(obj, a_class):
     """
         function that checks if the class is an instance of the specified class
         Args:
-            obj: an instance
+            obj: useed to check if its an instance of the specified class
             a_class: the class to check
         Returns:
             True if the class is an instance of the specified class otherwise False
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,11 +3,12 @@
     defines the function is_kind_of_class
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
      if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class
      Args:
-        obj: the object to check for
+        obj: the object to check for its a kind of class
         a_class: the class to check if the object is its instance
     Returns:
         true if the object is an instance of the specified class or its subclass
