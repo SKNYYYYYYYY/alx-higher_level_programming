6cf45b26798a0144cc2c10dedd457ad7647073e2
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-15 01:13:41 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+"""
+class MyInt(int):
+    """class MyInt"""
+    def __eq__(self, other):
+        """return True if the other is a MyInt"""
+        return super().__ne__(other)
+    
+    def __ne__(self, other):
+        """return False if the other is not a MyInt"""
+        return super().__eq__(other)
