9f5c6f714d96fee2bf3630aed418bd4c45ba919e
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-15 01:15:15 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This is a module that contains  a class
+"""
+def add_attribute(obj, attribute, value):
+    """Adds a new attribute"""
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
