e478b5b9e3995a6cfd634f11f8532d50182a47b2
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-14 23:02:36 +0300

    Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Class Relationship Checker Module
 
+This module provides utility functions for checking relationships
+between objects and classes. It defines the following function:
+
+- is_kind_of_class: Determines if an object is an instance of a given class
+                    or any of its subclasses.
+
+Usage:
+    from module_name import is_kind_of_class
+    result = is_kind_of_class(obj, class_name)
+"""
 def is_kind_of_class(obj, a_class):
     """
     Checks if the object is an instance of, or if the object is an
