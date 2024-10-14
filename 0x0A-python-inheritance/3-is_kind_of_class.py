6db08afdde3d3c152ff69f0bd32b7d318fd5b365
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-14 23:06:40 +0300

    Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,9 +8,6 @@ between objects and classes. It defines the following function:
 - is_kind_of_class: Determines if an object is an instance of a given class
                     or any of its subclasses.
 
-Usage:
-    from module_name import is_kind_of_class
-    result = is_kind_of_class(obj, class_name)
 """
 
 
@@ -18,7 +15,7 @@ Usage:
 def is_kind_of_class(obj, a_class):
     """
     Checks if the object is an instance of, or if the object is an
-    instance of a class that inherited from, the specified class.
+    instance of a class that inherited , the specified class.
 
     Args:
         obj: the object to check
