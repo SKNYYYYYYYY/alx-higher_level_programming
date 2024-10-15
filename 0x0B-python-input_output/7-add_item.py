bc9a4d6835def2307c6b65c73aae4fd03360f46f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-15 22:43:12 +0300

    File handling

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-    This module is a script that adds the command line arguments to a list
-    and saves them to a JSON file.
-"""
 import sys
-import os
 
+# Import the functions
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# check if it exists
-if os.path.exists('add_item.json'):
-    object = load_from_json_file('add_item.json')
-else:
-    object = []
-object.extend(sys.argv[1:])
-save_to_json_file(object, 'add_item.json')
+filename = "add_item.json"
+
+try:
+    # Load the existing list from the file
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    # If the file doesn't exist, start with an empty list
+    items = []
+
+# Add all command-line arguments (excluding the script name) to the list
+items.extend(sys.argv[1:])
+
+# Save the updated list back to the file
+save_to_json_file(items, filename)
