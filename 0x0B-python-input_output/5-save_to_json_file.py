2f68a4aa84f83cc6ebe29f1be24d000302ef6b94
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-15 21:00:33 +0300

    File handling

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines the function 
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as f:
+            json.dump(my_obj, f)
