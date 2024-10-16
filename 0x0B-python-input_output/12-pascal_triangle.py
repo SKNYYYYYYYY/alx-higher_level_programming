c5ebd76875a6cc4557358aea714072e134214f20
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-16 22:45:57 +0300

    Pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines the function pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """Function to generate a triangle"""
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
+    return triangle
