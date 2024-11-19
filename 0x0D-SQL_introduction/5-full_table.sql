78c8c0b88a1ac1e32e1ba0f8e16cc930370c4fe7
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-11-19 23:14:38 +0300

    task 0 - listing all databases main

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
--- Script to print full table description without using DESCRIBE or EXPLAIN
+-- Script to print full table
+-- description without using DESCRIBE or EXPLAIN
 show create table first_table
