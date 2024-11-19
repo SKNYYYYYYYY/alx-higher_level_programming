56be732e86f20401488c45d2ea57756ab390de44
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-11-19 23:09:19 +0300

    task 0 - listing all databases main

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
--- full description
-SHOW CREATE first_table
+-- Script to print full table description without using DESCRIBE or EXPLAIN
+SELECT `CREATE TABLE` FROM information_schema.tables 
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
