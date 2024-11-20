39ffcddf9f24127f1b7a5a93cc6f4f65ea9791d3
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-11-20 19:08:28 +0300

    basic mysql commands main

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
---delete
+-- delete
 DELETE FROM second_table WHERE score <= 5
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
---average
+-- average
 ALTER TABLE second_table
 ADD COLUMN average DECIMAL(10,4);
 INSERT INTO second_table (average) SELECT SUM(score) / COUNT(scores) FROM second_table;
