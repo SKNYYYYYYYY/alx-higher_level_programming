de23d3716a33f8d7ebc9f537fe952f044c1f6b5f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-11-21 23:31:48 +0300

    learning on user priviledges on mysql main

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create and read user
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2'
 CREATE USER IF NOT EXISTS 'user_0d_2'
-GRANT SELECT ON 'hbtn_0d_2.*' TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
