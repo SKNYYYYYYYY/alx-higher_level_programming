4b54ad750c9f0416d657f1c320c2bf3371070a02
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-11-22 00:09:20 +0300

    learning on TABLE AND DATABASE CREATION on mysql main

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create and read user
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2'
-CREATE USER IF NOT EXISTS 'user_0d_2'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_ pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
