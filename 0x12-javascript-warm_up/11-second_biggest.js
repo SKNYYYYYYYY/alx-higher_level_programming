c4ecd5932f7915c30028c5cd07a29c8df8a07727
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-12-17 11:25:26 +0300

    learning on jasascript scripting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,6 +10,6 @@ if (args.length === 1 || args.length === 2) {
   for (let i = 2; i < args.length; i++) {
     myArray.push(parseInt(args[i]));
   }
-  const sortedArray = [...new Set (myArray.sort((a, b) => b - a))];
+  const sortedArray = [...new Set(myArray.sort((a, b) => b - a))];
   console.log(sortedArray[sortedArray.length - 2]);
 }
