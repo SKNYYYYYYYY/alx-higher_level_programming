4e221657341a4038ae4a900657320f82ac62125a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-12-18 15:03:33 +0300

    learning javascript basics

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
-const process = require('process');
-const args = process.argv;
+let process = require('process');
+let args = process.argv;
 
 if (args.length === 1 || args.length === 2) {
-  console.log('0');
-} else {
-  const myArray = [];
+    console.log('0');
+}
+else {
+    let myArray = [];
 
-  for (let i = 2; i < args.length; i++) {
-    myArray.push(parseInt(args[i]));
-  }
-  const sortedArray = [...new Set(myArray.sort((a, b) => b - a))];
-  console.log(sortedArray[sortedArray.length - 2]);
+    for (let i = 2; i<args.length; i++) {
+        if (!myArray.includes(parseInt(args[i])) && !isNaN(parseInt(args[i]))) {
+            myArray.push(parseInt(args[i]));
+        }        
+    }
+    let sortedArray = myArray.sort();
+    console.log(sortedArray);
+    console.log(sortedArray[sortedArray.length - 2]);
 }
